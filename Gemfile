8ca71efcd3bd920af46f39c3710f193e76935d81
Patrick Pinto <ppinto@gowgates.com.au>
2024-12-03 13:59:15 +0000

    format

--- Gemfile
+++ Gemfile
@@ -60,5 +60,6 @@ end
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem 'capybara'
+  gem 'rspec-github', require: false
   gem 'selenium-webdriver'
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  before_action :find_user, only: :show
+  # before_action :find_user, only: :show
 
   def update
     @user = User.update('user_params')
-    params[:class].constantize.call(inputs)
+    # params[:class].constantize.call(inputs)
   end
 
   def user_params
-    params.require(:user).permit!
+    # params.require(:user).permit!
   end
 end
